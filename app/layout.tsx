637cdf1d2f6dfc83fc835448d6af605b27fa7af5
Djonahuti <djonah04@gmail.com>
2025-01-11 14:54:59 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 
 export const metadata: Metadata = {
   title: "U Bank",
-  description: "U Bank is a modern banking platform for everyone.",
+  description: "U Bank is an advanced banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
