ed84d6748bbe6fa6cdc8d3f01943309a9366c3f4
Djonahuti <djonah04@gmail.com>
2025-01-14 15:17:46 +0100

    commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { usePathname } from 'next/navigation';
+import Footer from './Footer';
 
 const Sidebar = ({user}: SiderbarProps) => {
     const pathname = usePathname();
@@ -44,7 +45,7 @@ const Sidebar = ({user}: SiderbarProps) => {
             USER
         </nav>
 
-        FOOTER
+        <Footer user={user} />
       </section>
     )
   }
