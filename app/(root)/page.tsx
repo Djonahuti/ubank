f83e308182dc4954d6ea785a62655a39e4d563f8
Djonahuti <djonah04@gmail.com>
2025-01-16 21:17:40 +0100

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,10 @@ import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react"
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import RecentTransactions from "@/components/RecentTransactions";
 
 const Home = async ({ searchParams: { id, page }}: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ userId: loggedIn.$id })
 
@@ -15,11 +17,6 @@ const Home = async ({ searchParams: { id, page }}: SearchParamProps) => {
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
   const account = await getAccount({ appwriteItemId})
-
-  console.log({
-    accountsData,
-    account
-  })
 
   return (
     <section className="home">
@@ -38,7 +35,12 @@ const Home = async ({ searchParams: { id, page }}: SearchParamProps) => {
           />
         </header>
 
-        RECENT TRANSACTIONS
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={accounts?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
 
       <RightSidebar
